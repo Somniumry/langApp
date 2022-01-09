71ac333b155c146dbeb464ec7e16f7475131dcbf
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2022-01-09 21:51:45 +0900

    4.13 parallel과 ratate 적용

--- App.js
+++ App.js
@@ -22,26 +22,16 @@ const Card = styled.View`
 const AnimatedCard = Animated.createAnimatedComponent(Card)
 
 export default function App() {
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, { dx }) => { position.setValue(dx) },
-      onPanResponderGrant: () => onPressIn(),
-      onPanResponderRelease: () => {
-        Animated.parallel([
-          onPressOut,
-          Animated.spring(position, {
-            toValue: 0,
-            useNativeDriver: true
-          }).start()
-        ])
-      }
-    })
-  ).current
-
+  // Values
   const scale = useRef(new Animated.Value(1)).current; // 사각형 크기
   const position = useRef(new Animated.Value(0)).current; // 사각형 위치(가운데)
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    extrapolate: "clamp"
+  })
 
+  // Animations
   const onPressIn = () => { // 사각형 눌렀을 때
     Animated.spring(scale, { toValue: 0.85, useNativeDriver: true }).start()
   }
@@ -50,12 +40,35 @@ export default function App() {
     Animated.spring(scale, { toValue: 1, useNativeDriver: true })
   }
 
+  const goCenter = () => { // 원래 위치로
+    Animated.spring(position, { toValue: 0, useNativeDriver: true })
+  }
+
+  // Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx }) => {
+        console.log(">>>", dx)
+        position.setValue(dx)
+      },
+      onPanResponderGrant: () => onPressIn(),
+      onPanResponderRelease: () => {
+        Animated.parallel([onPressOut, goCenter]).start()
+      }
+    })
+  ).current
+
+
+
+
+
   return (
     <Container>
       <AnimatedCard
         {...panResponder.panHandlers}
         style={{
-          transform: [{ scale }, { translateX: position}]
+          transform: [{ scale }, { translateX: position }, { rotateZ: rotation }]
         }}
       >
         <Icon2 name={"pizza-slice"} size={98} color={"black"} />
