2f184bfdbc379b33360af422ea381fce375bb65d
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2022-01-09 15:43:59 +0900

    4.6 interpolation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Animated, TouchableOpacity } from 'react-native';
+import { Animated, Easing, Pressable, TouchableOpacity } from "react-native";
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -17,28 +17,40 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 const App = () => {
-  const [up, setUp] = useState(false); // 컴포넌트
-  const Y = useRef(new Animated.Value(0)).current; // 애니메이션
+  console.log("실행")
+  const [up, setUp] = useState(false);
+  const Y_POSITION = useRef(new Animated.Value(300)).current;
   const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
     console.log("touch")
-    Animated.timing(Y, {
-      toValue: up ? 200 : -200,
-      useNativeDriver: true
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 300 : -300,
+      useNativeDriver: true,
+      duration: 1000,
     }).start(toggleUp)
   };
 
-  console.log(">>>", up)
+  const opacity = Y_POSITION.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.5, 1]
+  })
+
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: [100, 0],
+  });
 
   return (
     <Container>
-      <TouchableOpacity onPress={moveUp}>
+      <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            transform: [{ translateY: Y }],
+            opacity,
+            borderRadius,
+            transform: [{ translateY: Y_POSITION }],
           }}
         />
-      </TouchableOpacity>
+      </Pressable>
     </Container>
   )
 };
