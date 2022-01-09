f9ab6737bdb1ca615af04b014dd6ed18122386b3
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2022-01-09 14:35:30 +0900

    4.5 Animation And State

--- App.js
+++ App.js
@@ -8,9 +8,6 @@ const Container = styled.View`
   align-items: center;
 `
 
-// TouchableOpacity -> View 로 바꾼 이유
-// Box라는 컴포넌트에 TouchableOpacity기능과 Animated기능 2개를 넣어서 기능 실행이 좋지 않음
-// 그래서 View로 바꾸고 Box를 TouchableOpacity로 감싼다
 const Box = styled.View`
   background-color: tomato;
   width: 200px;
@@ -20,19 +17,26 @@ const Box = styled.View`
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 const App = () => {
-
-  const Y = new Animated.Value(0);
+  const [up, setUp] = useState(false); // 컴포넌트
+  const Y = new Animated.Value(0); // 애니메이션
+  const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
-    Animated.spring(Y, {
+    Animated.timing(Y, {
       toValue: 200,
-      bounciness: 50,
       useNativeDriver: true
-    }).start()
+    }).start(toggleUp)
   };
 
-  console.log(Y); // animation은 재렌더링을 하지 않기 때문에(state로 값을 넣지 않는다는 뜻) 0으로 표시
-  // Y값 확인
-  Y.addListener(()=> console.log(Y)); // 애니메이션 값 확인
+  // 현 코드 실행 설명
+  /*
+  moveUp 함수 실행
+   애니메이션(timing)실행 된 후 toggleUp 함수가 실행됨
+   toggleUp 함수는 컴포넌트 상태관리 변경에 대한 함수이다
+   -> toggleUp 함수를 실행할 때마다 컴포넌트가 재렌더링 된다는 뜻
+
+  그렇기 때문에 moveUp 함수를 실행하고 나온 결과는
+  Y위치 0 -> Y위치 200(animation) -> toggleUp 실행 -> 컴포넌트 재렌더링 -> Y=new Animated.Value(0) 재실행 -> Y위치 0
+  */
 
   return (
     <Container>
