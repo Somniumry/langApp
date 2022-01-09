0e3402126fde8a78e22af411ce0c55782f40888d
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2022-01-09 16:24:21 +0900

    4.7 more interpolation > Nomad code

--- App.js
+++ App.js
@@ -1,55 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 import { Animated, Easing, Pressable, TouchableOpacity } from "react-native";
-import styled from 'styled-components/native';
-
+import styled from "styled-components/native";
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-`
-
+`;
 const Box = styled.View`
   background-color: tomato;
   width: 200px;
   height: 200px;
-`
-
+`;
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
-const App = () => {
-  console.log("실행")
+export default function App() {
   const [up, setUp] = useState(false);
-  const Y_POSITION = useRef(new Animated.Value(0)).current;
+  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current;
   const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
-    console.log("touch")
-    Animated.timing(Y_POSITION, {
-      toValue: up ? -300 : 300,
+    Animated.timing(POSITION, {
+      toValue: up ? 300 : -300,
       useNativeDriver: false,
       duration: 1000,
-    }).start(toggleUp)
+    }).start(toggleUp);
   };
-
-  const opacity = Y_POSITION.interpolate({
-    inputRange: [-300, 0, 300],
-    outputRange: [1, 0.5, 1]
-  })
-
-  const borderRadius = Y_POSITION.interpolate({
+  const rotation = POSITION.y.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["-360deg", "360deg"],
+  });
+  const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
   });
-
-  const rotation = Y_POSITION.interpolate({
+  const bgColor = POSITION.y.interpolate({
     inputRange: [-300, 300],
-    outputRange: ["-360deg", "360deg"]
-  })
-
-  const bgColor = Y_POSITION.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"]
-  })
-
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
+  });
   return (
     <Container>
       <Pressable onPress={moveUp}>
@@ -57,14 +43,10 @@ const App = () => {
           style={{
             borderRadius,
             backgroundColor: bgColor,
-            transform: [
-              { rotateY: rotation },
-              { translateY: Y_POSITION }],
+            transform: [{ rotateY: rotation }, { translateY: POSITION.y }],
           }}
         />
       </Pressable>
     </Container>
-  )
-};
-
-export default App;
+  );
+}
