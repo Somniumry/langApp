7e5f646548e7f501e958ca0734e0c3349a61a60a
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2022-01-09 16:15:16 +0900

    4.7 more interpolation

--- App.js
+++ App.js
@@ -19,13 +19,13 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 const App = () => {
   console.log("실행")
   const [up, setUp] = useState(false);
-  const Y_POSITION = useRef(new Animated.Value(300)).current;
+  const Y_POSITION = useRef(new Animated.Value(0)).current;
   const toggleUp = () => setUp((prev) => !prev);
   const moveUp = () => {
     console.log("touch")
     Animated.timing(Y_POSITION, {
       toValue: up ? 300 : -300,
-      useNativeDriver: true,
+      useNativeDriver: false,
       duration: 1000,
     }).start(toggleUp)
   };
@@ -40,14 +40,26 @@ const App = () => {
     outputRange: [100, 0],
   });
 
+  const rotation = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["-360deg", "360deg"]
+  })
+
+  const bgColor = Y_POSITION.interpolate({
+    inputRange: [-300, 300],
+    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"]
+  })
+
   return (
     <Container>
       <Pressable onPress={moveUp}>
         <AnimatedBox
           style={{
-            opacity,
             borderRadius,
-            transform: [{ translateY: Y_POSITION }],
+            backgroundColor: bgColor,
+            transform: [
+              { rotateY: rotation },
+              { translateY: Y_POSITION }],
           }}
         />
       </Pressable>
